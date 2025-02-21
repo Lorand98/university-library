841b79bec9f0ff5261f4205c354f30aca73a6608
Lorand98 <kalmarlorand@yahoo.com>
2025-02-21 23:04:59 +0200

    finish home page basic setup

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,18 @@
-import { Button } from "@/components/ui/button";
+import BookList from "@/components/BookList";
+import BookOverview from "@/components/BookOverview";
+import { sampleBooks } from "../constants";
 
 const Home = () => {
-    return <Button>
-        Click me
-    </Button>
+    return <>
+    <BookOverview
+    {...sampleBooks[0]}
+    />
+    <BookList
+    title="Latest Books"
+    books={sampleBooks}
+    containerClassName="mt-28"
+    />
+    </>
 }
 
 export default Home;
